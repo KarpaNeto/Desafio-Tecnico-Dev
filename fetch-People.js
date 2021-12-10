51685cb1293a2f74d7509d4cabf1af8060f87bf9
Pedro Krapinski Neto <pedrokn@gmail.com>
2021-12-10 08:09:31 -0300

    commit

--- fetch-People.js
+++ fetch-People.js
@@ -1,15 +1,16 @@
-const starWarsPeopleList = document.querySelector('.card');
+const starWarsPeopleList = document.querySelector('.card__list');
 fetch('https://swapi.dev/api/people')
     .then(function (response) {
         return response.json();
     })
     .then(function (json) {
         let people = json.results;
+        console.log(people);
 
         for (item of people) {
             console.log(item)
             let listItem = document.createElement('li');
-            listItem.innerHTML = `<img class="card-image" src="https://starwars-visualguide.com/assets/img/characters/1.jpg"/>
+            listItem.innerHTML = `<img class="card__image" src="https://starwars-visualguide.com/assets/img/characters/1.jpg"/>
              <h2>${item.name}</h2>
              <h3>Height: ${item.height}<h3>
              <h3>Mass: ${item.mass}<h3>
